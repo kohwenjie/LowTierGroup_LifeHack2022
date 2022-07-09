282d2c4f019fffdd512ebd4c2174856e8082466a
Kevin Tan YJ <kevintanyj@gmail.com>
2022-07-09 15:19:47 +0800

    geodesic distance computation added

--- src/components/ListBinsLocations.js
+++ src/components/ListBinsLocations.js
@@ -10,6 +10,7 @@ export default function ListBins() {
 
     useEffect(() => {
         getBinsLocation()
+        nearestBins(1.2863437, 103.8319802, 2000)
     }, []);
 
     useEffect(() => {
@@ -32,14 +33,40 @@ export default function ListBins() {
             .catch(error => console.log(error.message))
     }
 
-    function getGeoDesicDistance() {
-        
+    function nearestBins(userLat, userLon, distanceX) {
+        var selectedBins = [];
+        locationView.map(loc => {
+            var binLat = loc.data.Location._lat;
+            var binLon = loc.data.Location._long;
+            var distance = getGeodesicDistance(userLat, userLon, binLat, binLon);
+            if (distance <= distanceX) {
+                console.log([binLat, binLon])
+                selectedBins.push([binLat, binLon]);
+            }
+        })
+        return selectedBins;
+    }
+
+    function getGeodesicDistance(userLat, userLon, binsLat, binsLon) {
+        var radius = function(x) {
+            return x * Math.PI / 180;
+        };
+          
+        var R = 6378137; // Earth’s mean radius in meter
+        var dLat = radius(userLat - binsLat);
+        var dLong = radius(userLon - binsLon);
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(radius(userLat)) * Math.cos(radius(binsLat)) *
+            Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        return d; // returns the distance in metres
     }
 
     return (
         <div>
             <h4>
-                ListBins
+                List of bins location:
                 <ul>
                     {locationView.map(loc => (
                         <li key={loc.id}>{ [loc.id, ": [", loc.data.Location._lat, ", ", loc.data.Location._long, "]"]}</li>
